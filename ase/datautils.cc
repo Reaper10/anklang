9a9a842158bb1dfbb4288ddeffafe1441c24c7e9
Reaper10 <sshep89@yahoo.com>
2023-05-11 16:01:41 -0500

    Delete datautils.cc

--- ase/datautils.cc
+++
@@ -1,8 +0,0 @@
-// This Source Code Form is licensed MPL-2.0: http://mozilla.org/MPL/2.0
-#include "datautils.hh"
-
-namespace Ase {
-
-float const_float_zeros[AUDIO_BLOCK_FLOAT_ZEROS_SIZE] = { 0, /*...*/ };
-
-} // Ase
